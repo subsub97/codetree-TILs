a7bf8d9b1fe263846e03d4fafc934826a6bc4ed6
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2023-12-22 20:24:50 +0900

    Title: 키를 기준으로 정렬, Time: 256ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 231222/키를 기준으로 정렬/sort-by-height.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+    class Student {
+        public String name;
+        public int weight;
+        public int height;
+
+        Student(String name, int height, int weight) {
+            this.name = name;
+            this.height = height;
+            this.weight = weight;
+        }
+    };
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Student[] students = new Student[n];
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            String name = st.nextToken();
+            int height = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            students[i] = new Student(name,height,weight);
+        }
+
+        Arrays.sort(students, (a,b) -> a.height - b.height);
+
+        for(Student s : students) {
+            System.out.println(s.name + " " + s.height + " " + s.weight);
+        }
+    }
+}
