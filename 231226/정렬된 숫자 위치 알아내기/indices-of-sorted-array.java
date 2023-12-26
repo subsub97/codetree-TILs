aae7008936fb579ef72c86934782dbd6d8f479d8
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2023-12-26 21:35:00 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 315ms, Memory: 14MB, Status: Passed - CodeTree

--- 231226/정렬된 숫자 위치 알아내기/indices-of-sorted-array.java
+++ 231226/정렬된 숫자 위치 알아내기/indices-of-sorted-array.java
@@ -1,14 +1,19 @@
 import java.util.*;
 import java.io.*;
 
-// """
-// 풀이
-// 1. 최초 배열에 입력받은 인덱스 위치와 값을 기억한다.
-// 2. 값을 기준으로 정렬을 한다.
-// 3. 정렬된 위치를 기록한다.
-// 4. 최초 인덱스 위치를 기준으로 정렬한다. 
-// 5. 순서대로 3번에서 기록한 값을 출력한다.
-// """
+/*
+풀이
+1. 최초 배열에 입력받은 인덱스 위치와 값을 기억한다.
+2. 값을 기준으로 정렬을 한다.
+3. 정렬된 위치를 기록한다.
+4. 최초 인덱스 위치를 기준으로 정렬한다. 
+5. 순서대로 3번에서 기록한 값을 출력한다.
+
+개선된 풀이
+1. 최초 배열에 입력받은 인덱스 위치와 값을 기억한다.
+2. 값을 기준으로 정렬
+3. 정렬된 값을 순회하면서 최초 인덱스 위치에 정렬된 값의 인덱스를 넣는다.
+*/
 class NumberLocation {
     int initIndex;
     int sortingIndex = 0;
@@ -24,10 +29,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         final int n;
         NumberLocation[] numberList;
+        int answer[];
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine().trim());
         numberList = new NumberLocation[n];
+        answer = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         for(int i = 0; i < n; i++) { //주어진 숫자번호와 인덱스 위치를 배열에 추가
@@ -38,14 +45,11 @@ public class Main {
         Arrays.sort(numberList, (a,b) -> a.number - b.number);
 
         for(int i = 0; i < n; i++) {
-            // 정렬이된 index를 기록
-            numberList[i].sortingIndex = i+1;
+            answer[numberList[i].initIndex] = i + 1;
         }
-
-        Arrays.sort(numberList, (a,b) -> a.initIndex - b.initIndex);
         
-        for(NumberLocation num: numberList) {
-            System.out.print(num.sortingIndex + " ");
+        for(int index: answer) {
+            System.out.print(index + " ");
         }
     }
 }
