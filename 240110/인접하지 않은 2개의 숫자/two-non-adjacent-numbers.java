f2821e8540754f410ae666b06f87ed5f24cae1cf
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-10 16:58:03 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

--- 240110/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.java
+++ 240110/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.java
@@ -1,27 +1,28 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-        int n = Integer.parseInt(br.readLine().trim());
-        int[]  numArray = new int[n];
-        int maxSum = 0;
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i <n; i++) {
-            numArray[i] = Integer.parseInt(st.nextToken());
-        }
+		int max = 0;
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i+2; j < n; j++) {
-                if(numArray[i] + numArray[j] > maxSum) {
-                    maxSum = numArray[i] + numArray[j];
-                }
-            }
-        }
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
 
-        System.out.print(maxSum);
-    }
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 2; j < n; j++) {
+				int sum = arr[i] + arr[j];
+				max = Math.max(max, sum);
+			}
+		}
+		System.out.println(max);
+	}
+
 }
