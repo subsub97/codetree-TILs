a2fc03a60215b02bd65365b40b3ac74dffab21ac
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-10 22:09:21 +0900

    Title: G or H 3, Time: 150ms, Memory: 9MB, Status: Passed - CodeTree

--- 240110/G or H 3/G-or-H-3.java
+++ 240110/G or H 3/G-or-H-3.java
@@ -1,37 +1,42 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    private static final int INT_MIN = Integer.MIN_VALUE;
-    private static int MAX_L = 10000;
+    public static final int MAX_SIZE = 10000;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stk = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(stk.nextToken());
-        int K = Integer.parseInt(stk.nextToken()) + 1; // 사진의 크기는 x 값끼리의 차 -> 예시) 0, 6 -> 0, 1, 2, 3, 4, 5, 6인 총 7개 좌표 탐색
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int len = MAX_L;
-        int[] locations = new int[N];
-        int[] alphabets = new int[N];
-        int[] lines = new int[len];
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int location;
+        int ans = 0;
+        char initial;
 
-        for(int i = 0; i < N; i++) {
-            stk = new StringTokenizer(br.readLine());
-            locations[i] = Integer.parseInt(stk.nextToken());
-            char alphabet = stk.nextToken().charAt(0);
-            alphabets[i] = alphabet == 'G' ? 1 : 2;
-            lines[locations[i] - 1] = alphabets[i];
-        } 
+        char[] line = new char[MAX_SIZE];
 
-        int max = INT_MIN;
-        for(int i = 0; i <= len - K; i++) {
-            int sum = 0;
-            for(int j = i; j < i + K; j++)
-                sum += lines[j];
-            max = Math.max(max, sum);
+        for(int i = 0 ; i < N; i++) {
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+
+            location = Integer.parseInt(st1.nextToken());
+            initial = st1.nextToken().charAt(0);
+            line[location] = initial;
         }
 
-        System.out.print(max);
+        for(int i = 0; i < MAX_SIZE - K; i++){
+            int currentScore = 0;
+        
+            for(int j = i; j <= K+i; j++) {
+                if(line[j] == 'G') {
+                    currentScore += 1;
+                }
+                else if(line[j] == 'H') {
+                    currentScore += 2;
+                }
+            }
+            ans = Math.max(ans,currentScore);
+        }
+        System.out.print(ans);
     }
 }
