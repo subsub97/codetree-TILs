de0f6b2daa1ccfb394a63cddeae763abfb49b9fb
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-10 22:22:29 +0900

    Title: 두 번째를 첫 번째로, Time: 79ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240110/두 번째를 첫 번째로/second-to-first.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = br.readLine();
+        char[] convertWord = new char[word.length()];
+
+        char firstChar = word.charAt(0);
+        char secondChar = word.charAt(1);
+
+        for(int i = 0; i < word.length(); i++) {
+            if(word.charAt(i) == secondChar) {
+                convertWord[i] = firstChar;
+            }
+            convertWord[i] = word.charAt(i);
+        }
+
+        System.out.print(String.valueOf(convertWord));
+
+    }
+}
