4dc756ff1b70453cbdb41d3063ab9cce79fea257
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-11 21:21:14 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240111/두 가지로 열리는 자물쇠/a-two-way-lock.java
+++ 240111/두 가지로 열리는 자물쇠/a-two-way-lock.java
@@ -10,23 +10,23 @@ public class Main {
         StringTokenizer st1 = new StringTokenizer(br.readLine());
         StringTokenizer st2 = new StringTokenizer(br.readLine());
 
-        int[] numbers = new int[n];
+        int[] numbers = new int[n+1];
         int[] A = new int[3];
         int[] B = new int[3];
 
         for(int i = 0; i < 3; i++) {
-            A[i] = Integer.parseInt(st1.nextToken()) - 1;
+            A[i] = Integer.parseInt(st1.nextToken());
         }
 
         for(int i = 0; i < 3; i++) {
-            B[i] = Integer.parseInt(st2.nextToken()) - 1;
+            B[i] = Integer.parseInt(st2.nextToken());
         }
 
         for(int i = 0; i < n; i++) {
             numbers[i] = i+1;
         }
 
-        int[][] possibleNumber = new int[3][n];
+        int[][] possibleNumber = new int[3][n+1];
 
         //겹치는 경우를 빼주면 된다.
         for(int i =0; i < 3; i++) {
@@ -34,7 +34,7 @@ public class Main {
             possibleNumber[i][B[i]] = 1;
 
             for(int j = 1; j < 3; j++) {
-                int upNumber = (A[i] + j) % (n-1);
+                int upNumber = (A[i] + j) % (n);
                 int downNumber = (A[i] - j) < 0 ? (A[i] - j) + n : (A[i] - j);
 
                 possibleNumber[i][upNumber] = 1;
@@ -42,8 +42,8 @@ public class Main {
             }
 
             for(int j = 1; j < 3; j++) {
-                int upNumber = (B[i] + j) % (n-1);
-                int downNumber = (B[i] - j) < 0 ? (B[i] - j) + n : (B[i] - j);
+                int upNumber = (B[i] + j) % (n);
+                int downNumber = (B[i] - j) <= 0 ? (B[i] - j) + n : (B[i] - j);
 
                 possibleNumber[i][upNumber] = 1;
                 possibleNumber[i][downNumber] = 1;
@@ -60,8 +60,13 @@ public class Main {
             }
             ans *= (10 - cnt);
         }
-
-        System.out.print(250 - ans);
+        if(n < 5){
+            System.out.print(n * n *n);
+        }
+        else{
+            System.out.print(250 - ans);
+        }
+        
 
     }
 }
