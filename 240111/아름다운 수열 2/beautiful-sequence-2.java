e61343108cf370f8cd6d686af907105a0d151a34
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-11 15:29:59 +0900

    Title: 아름다운 수열 2, Time: 109ms, Memory: 8MB, Status: Passed - CodeTree

--- 240111/아름다운 수열 2/beautiful-sequence-2.java
+++ 240111/아름다운 수열 2/beautiful-sequence-2.java
@@ -51,6 +51,7 @@ public class Main {
                         if(sectionNumbers[k] == B[l] && used[l] == false) {
                             used[l] = true;
                             check = true;
+                            break;
                         }
                     }
                     if(!check) {
