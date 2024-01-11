0bba40b9ec4e2e48a9efcbc5220974d9b6a4eef6
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-11 16:08:46 +0900

    Title: 바구니 안의 사탕 2, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240111/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_SIZE = 100;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] basketLocation = new int[MAX_SIZE];
+        
+        for(int i = 0; i < N; i++) {
+            StringTokenizer candyInfoToken = new StringTokenizer(br.readLine());
+            int candyAmount = Integer.parseInt(candyInfoToken.nextToken());
+            int location = Integer.parseInt(candyInfoToken.nextToken());
+
+            basketLocation[location] = candyAmount;
+        }
+
+        int ans = Integer.MIN_VALUE;
+
+        for(int i = K; i < MAX_SIZE - K; i++) {
+            int curSum = 0;
+            for(int j = i - K; j <= i + K; j++) {
+                curSum += basketLocation[j];
+            }
+            ans = Math.max(ans, curSum);
+        }
+        System.out.print(ans);
+    }
+}
