bade34e3cf51e51c5425a3e20a0a5b8421aaca63
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-11 22:19:47 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240111/두 가지로 열리는 자물쇠/a-two-way-lock.java
+++ 240111/두 가지로 열리는 자물쇠/a-two-way-lock.java
@@ -1,71 +1,58 @@
-import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new  BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine().trim());
-
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-
-        int[] numbers = new int[n+1];
-        int[] A = new int[3];
-        int[] B = new int[3];
-
-        for(int i = 0; i < 3; i++) {
-            A[i] = Integer.parseInt(st1.nextToken());
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int N=sc.nextInt();
+        int [] first=new int[3];
+        int [] second=new int[3];
+        for (int j=0; j<3; j++){
+            first[j]=sc.nextInt();
+        }
+        for (int j=0; j<3; j++){
+            second[j]=sc.nextInt();
         }
 
-        for(int i = 0; i < 3; i++) {
-            B[i] = Integer.parseInt(st2.nextToken());
+        if (N<=5){
+            System.out.println(N*N*N);
+            return;
         }
 
-        for(int i = 0; i < n; i++) {
-            numbers[i] = i+1;
-        }
+        int[][] first_can = new int[3][5];
+        int[][] second_can = new int[3][5];
 
-        int[][] possibleNumber = new int[3][n+1];
-
-        //겹치는 경우를 빼주면 된다.
-        for(int i =0; i < 3; i++) {
-            possibleNumber[i][A[i]] = 1;
-            possibleNumber[i][B[i]] = 1;
-
-            for(int j = 1; j < 3; j++) {
-                int upNumber = (A[i] + j) > n ? (A[i] + j) % n  : (A[i] + j) % (n+1);
-                int downNumber = (A[i] - j) <= 0 ? (A[i] - j) + n : (A[i] - j);
-
-                possibleNumber[i][upNumber] = 1;
-                possibleNumber[i][downNumber] = 1;
+        for (int i=0; i<3; i++){
+            for (int k=-2; k<3; k++) {
+                if (first[i] + k <= 0) {
+                    first_can[i][k + 2] = N + first[i] + k;
+                }
+                else{
+                    first_can[i][k+2]=first[i]+k;
+                    }
+                if(second[i]+k<=0){
+                    second_can[i][k+2]=N+first[i]+k;
+                }
+                else{
+                    second_can[i][k+2]=second[i]+k;
+                }
             }
-
-            for(int j = 1; j < 3; j++) {
-                int upNumber = (B[i] + j) > n ? (B[i] + j) % n  : (B[i] + j) % (n+1);
-                int downNumber = (B[i] - j) <= 0 ? (B[i] - j) + n : (B[i] - j);
-
-                possibleNumber[i][upNumber] = 1;
-                possibleNumber[i][downNumber] = 1;
+        }
+        int cnt=0;
+        int sum=1;
+        for(int i=0; i<3; i++){
+            for (int j=0; j<5; j++){
+                for (int k=0; k<5; k++){
+                    if(first_can[i][j]==second_can[i][k]){
+                        cnt+=1;
+                    }
+                }
             }
-
-
-        }
-
-        int ans = 1;
-        for(int i =0; i < 3; i++) {
-            int cnt = 0;
-            for(int j = 0; j <= n; j++) {
-                if(possibleNumber[i][j] == 1) cnt++;
+            if(cnt!=0) {
+                sum *= cnt;
             }
-            ans *= (10 - cnt);
+            cnt=0;
         }
+        System.out.println(250-sum);
 
-        if(n < 5){
-            System.out.print(n * n *n);
-        }
-        else{
-            System.out.print(250 - ans);
-        }
     }
 }
