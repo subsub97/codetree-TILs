658fb77b84a5ce19e0822744a873a2d9880cb5d0
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-12 23:44:09 +0900

    Title: 스승의 은혜 3, Time: 123ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240112/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int budget = Integer.parseInt(st.nextToken());
+        
+        int[] prices = new int[n];
+        int[] fees = new int[n];
+
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+
+            prices[i] = Integer.parseInt(st1.nextToken());
+            fees[i] = Integer.parseInt(st1.nextToken());
+        }
+        
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            int curSum = budget;
+            int cnt = 0;
+            for(int j = 0; j < n; j++) {  
+                if(i == j) {
+                    if(curSum >= prices[j]/2 + fees[j]){
+                        curSum -= prices[j] / 2;
+                        curSum -= fees[j];
+                        cnt++;
+                    }
+                }
+                else{
+                    if(curSum >= prices[j] + fees[j]){
+                        curSum -= (prices[j] + fees[j]);
+                        cnt++;
+                    }
+                }
+            }
+            ans = Math.max(ans,cnt);
+        }
+
+        System.out.print(ans);
+    }
+}
