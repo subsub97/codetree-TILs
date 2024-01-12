451e144fd130da42ee36449702297aca4fae070f
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-12 23:53:08 +0900

    Title: 스승의 은혜 3, Time: 128ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240112/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 240112/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -10,16 +10,22 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int budget = Integer.parseInt(st.nextToken());
         
-        int[] prices = new int[n];
-        int[] fees = new int[n];
+        int[][] prices = new int[n][2];
+        
 
 
         for(int i = 0; i < n; i++) {
             StringTokenizer st1 = new StringTokenizer(br.readLine());
 
-            prices[i] = Integer.parseInt(st1.nextToken());
-            fees[i] = Integer.parseInt(st1.nextToken());
+            prices[i][0] = Integer.parseInt(st1.nextToken());
+            prices[i][1] = Integer.parseInt(st1.nextToken());
         }
+
+        Arrays.sort(prices, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[0],b[0]);
+            }
+        });
         
         int ans = 0;
 
@@ -28,15 +34,15 @@ public class Main {
             int cnt = 0;
             for(int j = 0; j < n; j++) {  
                 if(i == j) {
-                    if(curSum >= (prices[j]/2 + fees[j])){
-                        curSum -= prices[j] / 2;
-                        curSum -= fees[j];
+                    if(curSum >= (prices[j][0]/2 + prices[j][0])){
+                        curSum -= prices[j][0] / 2;
+                        curSum -= prices[j][1];
                         cnt++;
                     }
                 }
                 else{
-                    if(curSum >= prices[j] + fees[j]){
-                        curSum -= (prices[j] + fees[j]);
+                    if(curSum >= prices[j][0] + prices[j][1]){
+                        curSum -= (prices[j][0] + prices[j][1]);
                         cnt++;
                     }
                 }
