36799f0b11ae99ad025fd1f5c58b26f93bd6e249
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-13 00:00:59 +0900

    Title: 스승의 은혜 3, Time: 132ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240113/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int budget = Integer.parseInt(st.nextToken());
+        
+        int[][] prices = new int[n][2];
+        
+
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+
+            prices[i][0] = Integer.parseInt(st1.nextToken());
+            prices[i][1] = Integer.parseInt(st1.nextToken());
+        }
+
+        Arrays.sort(prices, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[0],b[0]);
+            }
+        });
+        
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            int curSum = budget;
+            int cnt = 0;
+            for(int j = 0; j < n; j++) {  
+                if(i == j) {
+                    if(curSum >= ((prices[j][0]/2) + prices[j][1])){
+                        curSum -= prices[j][0] / 2;
+                        curSum -= prices[j][1];
+                        cnt++;
+                    }
+                }
+                else{
+                    if(curSum >= prices[j][0] + prices[j][1]){
+                        curSum -= (prices[j][0] + prices[j][1]);
+                        cnt++;
+                    }
+                }
+            }
+            ans = Math.max(ans,cnt);
+        }
+
+        System.out.print(ans);
+    }
+}
