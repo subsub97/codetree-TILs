4b63cfe23596ba7135df7748a932cafacef61482
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-12 11:57:58 +0900

    Title: 삼각형 만들기, Time: 107ms, Memory: 9MB, Status: Passed - CodeTree

--- 240112/삼각형 만들기/create-triangle.java
+++ 240112/삼각형 만들기/create-triangle.java
@@ -30,30 +30,10 @@ public class Main {
                     
                     if((y1 == y2 || y1 == y3 || y2 == y3)
                         && (x1 == x2 || x1 == x3 || x2 == x3)) {
-                            int xLen = 0;
-                            int yLen = 0;
 
-                            if(x1 == x2) {
-                                yLen = Math.abs(y1 - y2);
-                            }
-                            else if(x1 == x3){
-                                yLen = Math.abs(y1 - y3);
-                            }
-                            else if(x2 == x3) {
-                                yLen = Math.abs(y2 - y3);
-                            }
 
-                            if(y1 == y2) {
-                                xLen = Math.abs(x1 - x2);
-                            }
-                            else if(y1 == y3) {
-                                xLen = Math.abs(x1 - x3);
-                            }
-                            else if(y2 == y3) {
-                                xLen = Math.abs(x2 - x3);
-                            }
-
-                            ans = Math.max(ans,xLen * yLen);
+                            ans = Math.max(ans,Math.abs((xs[i] * ys[j] + xs[j] * ys[k] + xs[k] * ys[i]) - 
+                        (xs[j] * ys[i] + xs[k] * ys[j] + xs[i] * ys[k])));
                     }
                 }
             }
