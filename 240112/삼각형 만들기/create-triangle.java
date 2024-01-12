302fb0454d0f5417ba4a471f26ab62c43168c651
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-12 11:39:25 +0900

    Title: 삼각형 만들기, Time: 101ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240112/삼각형 만들기/create-triangle.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine().trim());
+        
+        int[] xs = new int[n];
+        int[] ys = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            xs[i] = Integer.parseInt(st.nextToken());
+            ys[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int ans = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j < n ; j++) {
+                for(int k = j + 1; k < n; k++) {
+                    int x1 = xs[i], x2 = xs[j], x3 = xs[k];
+                    int y1 = ys[i], y2 = ys[j], y3 = ys[k];
+                    
+                    if((y1 == y2 || y1 == y3 || y2 == y3)
+                        && (x1 == x2 || x1 == x3 || x2 == x3)) {
+                            int xLen = 0;
+                            int yLen = 0;
+
+                            if(x1 == x2) {
+                                yLen = Math.abs(y1 - y2);
+                            }
+                            else if(x1 == x3){
+                                yLen = Math.abs(y1 - y3);
+                            }
+                            else if(x2 == x3) {
+                                yLen = Math.abs(y2 - y3);
+                            }
+
+                            if(y1 == y2) {
+                                xLen = Math.abs(x1 - x2);
+                            }
+                            else if(y1 == y3) {
+                                xLen = Math.abs(y1 - y3);
+                            }
+                            else if(y2 == y3) {
+                                xLen = Math.abs(y2 - y3);
+                            }
+
+                            ans = Math.max(ans,xLen * yLen);
+                    }
+                }
+            }
+        }
+        System.out.print(ans);
+    }
+}
