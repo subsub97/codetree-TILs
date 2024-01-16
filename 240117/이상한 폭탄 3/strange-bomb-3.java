afb3b03de545769ed420da33868d0d31044b788b
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-17 01:13:57 +0900

    Title: 이상한 폭탄 3, Time: 266ms, Memory: 19MB, Status: Wrong Answer - CodeTree

---
+++ 240117/이상한 폭탄 3/strange-bomb-3.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int maxNumber = 0;
+        int[] bombs = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            bombs[i] = Integer.parseInt(br.readLine().trim());
+            maxNumber = Math.max(bombs[i], maxNumber);
+        }
+
+        int maxCnt = 0;
+        int ans = 0;
+        for (int i = 1; i <= maxNumber ; i++) {
+            // i번 폭탄이 몇개 터지는지 확인
+            boolean[] explosion = new boolean[n];
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if(bombs[j] == i) {
+                    for (int l = j; l <= k; l++) {
+                        if(bombs[l] == i && !explosion[l]) {
+                            explosion[l] = true;
+                            cnt++;
+                        }
+                    }
+                }
+            }
+            if(cnt > maxCnt) {
+                maxCnt = cnt;
+                ans = i;
+            }
+        }
+
+        System.out.println(ans);
+        
+    }
+}
