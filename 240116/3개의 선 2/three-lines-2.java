021572d9f43db8e3cc59cceac7dde4f533442d62
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-16 18:03:42 +0900

    Title: 3개의 선 2, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240116/3개의 선 2/three-lines-2.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class Main {
+    public static final int MAX_X = 10;
+    public static final int MAX_N = 20;
+    
+    public static int n;
+    public static int[] x = new int[MAX_N];
+    public static int[] y = new int[MAX_N];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+        }
+
+        int ans = 0;
+
+        // 모든 직선에 대해 전부 시도해 봅니다.
+        for(int i = 0; i <= MAX_X; i++)
+            for(int j = 0; j <= MAX_X; j++)
+                for(int k = 0; k <= MAX_X; k++) {
+                    // success : 직선 3개로 모든 점을 지나게 할 수 있으면 true
+                    boolean success = true;
+                    // x축에 평행한 직선 3개로
+                    // 모든 점을 지나게 할 수 있는 경우
+                    for(int l = 0; l < n; l++) {
+                        // 해당 점이 직선에 닿으면 넘어갑니다
+                        if(x[l] == i || x[l] == j || x[l] == k)
+                            continue;
+                        
+                        success = false;
+                    }
+                    if(success){
+                        ans = 1;
+                        
+                    }
+
+                    // x축에 평행한 직선 2개와 y축에 평행한 직선 1개로
+                    // 모든 점을 지나게 할 수 있는 경우
+                    success = true;
+                    for(int l = 0; l < n; l++) {
+                        // 해당 점이 직선에 닿으면 넘어갑니다
+                        if(x[l] == i || x[l] == j || y[l] == k)
+                            continue;
+                        
+                        success = false;
+                    }
+                    if(success)
+                        ans = 1;
+                    
+                    // x축에 평행한 직선 1개와 y축에 평행한 직선 2개로
+                    // 모든 점을 지나게 할 수 있는 경우
+                    success = true;
+                    for(int l = 0; l < n; l++) {
+                        // 해당 점이 직선에 닿으면 넘어갑니다
+                        if(x[l] == i || y[l] == j || y[l] == k)
+                            continue;
+                        
+                        success = false;
+                    }
+                    if(success)
+                        ans = 1;
+                    
+                    // y축에 평행한 직선 3개로
+                    // 모든 점을 지나게 할 수 있는 경우
+                    success = true;
+                    for(int l = 0; l < n; l++) {
+                        // 해당 점이 직선에 닿으면 넘어갑니다
+                        if(y[l] == i || y[l] == j || y[l] == k)
+                            continue;
+                        
+                        success = false;
+                    }
+                    if(success)
+                        ans = 1;
+                    
+                }
+
+        System.out.print(ans);
+    }
+}
