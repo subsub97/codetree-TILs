51e3658daab660083b526ac5b773b237b0e900f1
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-17 23:52:10 +0900

    Title: A, B, C, D 찾기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240117/A, B, C, D 찾기/find-a-b-c-d.java
@@ -0,0 +1,24 @@
+package codeTree;
+
+import com.sun.security.jgss.GSSUtil;
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] numbers = new int[15];
+        for (int i = 0; i < 15; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+
+        System.out.println(numbers[0] + " "+numbers[1] + " " + numbers[2] +" "+ (numbers[14] - (numbers[0] + numbers[1] + numbers[2])) );
+
+    }
+}
