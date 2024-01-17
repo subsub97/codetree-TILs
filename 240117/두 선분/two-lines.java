21ba003844bb85ff83dc82ad00347091bda63b2d
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-17 15:58:42 +0900

    Title: 두 선분, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240117/두 선분/two-lines.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static final int MAX_N = 100;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] x = new int[4];
+
+        String correct = "interesting";
+        String notCorrect = "noninterectiog";
+        for (int i = 0; i < 4; i++) {
+            x[i] = Integer.parseInt(st.nextToken());
+        }
+        if((x[0] <= x[2] && x[2] <= x[1]) || (x[0] <= x[3] && x[3] <= x[2])){
+            System.out.println(correct);
+        }
+        else {
+            System.out.println(notCorrect);
+        }
+
+    }
+}
