5c2a840323b8fea28f73bf25ba39990457f667a6
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-18 18:57:17 +0900

    Title: 이상한 수열, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240118/이상한 수열/a-strange-sequence.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+        System.out.print(f(n));
+    }
+
+    public static int f(int n){
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+
+        return n / 3 + f(n-1);
+    }
+}
