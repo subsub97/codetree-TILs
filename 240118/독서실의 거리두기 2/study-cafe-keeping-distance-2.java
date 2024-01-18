9ab68b35dce87e8d67bdde40744c4f4bfd4f9e6c
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-18 14:44:07 +0900

    Title: 독서실의 거리두기 2, Time: 123ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240118/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+public class Main {
+    public static int ans = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine().trim());
+        String seatInfo = br.readLine();
+
+        for (int i = 0; i < n; i++) {
+            // 좌석 지정
+            if(seatInfo.charAt(i) == '1') {
+                continue;
+            }
+            else{
+                String temp = seatInfo.substring(0,i) + "1" + seatInfo.substring(i+1);
+                findSeat(temp);
+            }
+        }
+        System.out.println(ans);
+    }
+
+    public static void findSeat(String seatInfo) {
+        int preDiff = 100;
+
+        for (int i = 0; i < seatInfo.length(); i++) {
+            if(seatInfo.charAt(i) == '1') {
+                int diff = 1;
+                for (int j = i+1; j < seatInfo.length(); j++) {
+                    if(seatInfo.charAt(j) == '1') {
+                        preDiff = Math.min(diff,preDiff);
+                        break;
+                    }
+                    diff++;
+                }
+            }
+        }
+        ans = Math.max(ans,preDiff);
+    }
+}
