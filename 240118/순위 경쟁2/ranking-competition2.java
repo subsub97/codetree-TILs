b05722f276983539be818b4848aba999c090f278
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-18 16:08:55 +0900

    Title: 순위 경쟁2, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240118/순위 경쟁2/ranking-competition2.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+
+        char[] hallOfFame = new char[1];
+
+        int a = 0;
+        int b = 0;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            char name = st.nextToken().charAt(0);
+            int score = Integer.parseInt(st.nextToken());
+
+            if(name == 'A') {
+                a += score;
+            }
+            else{
+                b += score;
+            }
+
+            if(a > b) {
+                if(hallOfFame[0] != 'A') {
+                    hallOfFame[0] = 'A';
+                    ans++;
+                }
+            } else if (b > a) {
+                if(hallOfFame[0] != 'B') {
+                    hallOfFame[0] = 'B';
+                    ans++;
+                }
+            }
+            else{
+                if(hallOfFame[0] != 'C') {
+                    hallOfFame[0] = 'C';
+                    ans++;
+                }
+            }
+        }
+        System.out.println(ans);
+
+    }
+}
