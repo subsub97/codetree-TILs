959d28dfadf0d10203bff927f8b51c1f87f64a09
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-18 23:04:46 +0900

    Title: 개발자의 가위바위보, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240118/개발자의 가위바위보/developer's-rock-paper-scissors.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+
+        int[] devA = new int[n];
+        int[] devB = new int[n];
+        String[][] gameRule = {{"가위", "바위", "보"},{"가위","보","바위"},
+                {"바위","가위","보"}, {"바위","보","가위"}, {"보","바위","가위"},{"보","가위","바위"}};
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            devA[i] = Integer.parseInt(st.nextToken());
+            devB[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                String a = gameRule[i][devA[j]-1];
+                String b = gameRule[i][devB[j]-1];
+
+                if(a.equals("가위") && b.equals("보")) {
+                    cnt++;
+                }
+                else if(a.equals("바위") && b.equals("가위")){
+                    cnt++;
+                }
+                else if(a.equals("보") && b.equals("바위")) {
+                    cnt++;
+                }
+            }
+            ans = Math.max(cnt, ans);
+        }
+
+        System.out.println(ans);
+
+
+
+    }
+}
