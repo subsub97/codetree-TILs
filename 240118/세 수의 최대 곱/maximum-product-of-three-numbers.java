aa0d9d25a9a308281b869b77e94f7904646df2f5
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-18 18:34:43 +0900

    Title: 세 수의 최대 곱, Time: 289ms, Memory: 19MB, Status: Passed - CodeTree

--- 240118/세 수의 최대 곱/maximum-product-of-three-numbers.java
+++ 240118/세 수의 최대 곱/maximum-product-of-three-numbers.java
@@ -1,35 +1,30 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 
 public class Main {
-    public static final int MAX_SIZE = 10;
-    public static char[][] grid = new char[MAX_SIZE][MAX_SIZE];
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] numbers = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int[] numbers = new int[n];
         for (int i = 0; i < n; i++) {
             numbers[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(numbers);
 
-        int maxNumber = Integer.MIN_VALUE;
+        int maxValue = Integer.MIN_VALUE;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                for (int k = j+1; k < n; k++) {
-                    maxNumber = Math.max(maxNumber, numbers[i] * numbers[j] * numbers[k]);
-                }
-            }
-        }
+        maxValue = Math.max(numbers[n - 1] * numbers[n - 2] * numbers[n - 3],maxValue);
+        maxValue = Math.max(numbers[0] * numbers[1] * numbers[n - 1], maxValue);
 
-        System.out.println(maxNumber);
+        System.out.println(maxValue);
     }
-
-
+    
 }
