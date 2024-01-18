35288f308c359812ff699490e88760b0773c1e80
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-19 01:33:06 +0900

    Title: 최대 H 점수 2, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240119/최대 H 점수 2/maximum-h-score-2.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+
+        int[] numbers = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+        
+        for(int i = 1; i < 100; i++) {
+            int cnt = 0;
+            int curL = l;
+            for(int j = n-1; j >= 0; j--){
+                if(numbers[j] + curL >= i){
+                    if(numbers[j] < i){
+                        curL -=  (i - numbers[j]);
+                    }
+                    cnt++;
+                    if(cnt >= i){
+                        break;
+                    }
+                }
+            }
+            if(cnt < i){
+                System.out.print(i-1);
+                break;
+            }
+        }
+    }
+}
