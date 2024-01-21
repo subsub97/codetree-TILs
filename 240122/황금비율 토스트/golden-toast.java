784a7aefacd4703116efd42f2430c69e7576205a
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-22 00:46:17 +0900

    Title: 황금비율 토스트, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240122/황금비율 토스트/golden-toast.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        LinkedList<Character> l = new LinkedList<>();
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        char[] code = br.readLine().toCharArray();
+
+        for(int i = 0; i < code.length;i++) {
+            l.add(code[i]);
+        }
+        ListIterator<Character> it = l.listIterator(l.size());
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            char cmd = st.nextToken().charAt(0);
+            if('L' == cmd) {
+                if(it.hasPrevious()) {
+                    it.previous();
+                }
+            }
+            else if('R' == cmd) {
+                if(it.hasNext()) {
+                    it.next();
+                }
+            }
+            else if('D' == cmd) {
+                if(it.hasNext()){
+                    it.next();
+                    it.remove();
+                }
+            }
+            else {
+                char codeAlpa = st.nextToken().charAt(0);
+                it.add(codeAlpa);
+            }
+        }
+
+        for(int i = 0; i < l.size(); i++) {
+            System.out.print(l.get(i));
+        }
+
+
+
+
+    }
+}
