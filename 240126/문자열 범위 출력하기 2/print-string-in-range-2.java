b4307b263c74af658b445d0b6661ed6c9a1ce994
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-01-26 23:59:15 +0900

    Title: 문자열 범위 출력하기 2, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

--- 240126/문자열 범위 출력하기 2/print-string-in-range-2.java
+++ 240126/문자열 범위 출력하기 2/print-string-in-range-2.java
@@ -11,8 +11,16 @@ public class Main {
         int sl = s.length();
         //sl 은 13 n 은 11 -1 더하면 
         //13부터 1까지 출력
+        if(n < sl) {
         for (int i = sl-1; i > sl - n -1; i--) {
             System.out.print(s.substring(i,i+1));
         }
+        }
+        else{
+        for (int i = sl-1; i > -1; i--) {
+            System.out.print(s.substring(i,i+1));
+        }
+        }
+
     }
 }
