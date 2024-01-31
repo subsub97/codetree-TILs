780ba1f7ed78a99c4241fff95eaf36410c129a3d
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-02-01 01:43:34 +0900

    Title: 금 채굴하기, Time: 96ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240201/금 채굴하기/gold-mining.java
+++ 240201/금 채굴하기/gold-mining.java
@@ -26,56 +26,26 @@ public class Main {
         }
         int maxCnt = 0;
         int preMaxCnt = 0;
-        int ans = 0;
-        for(int k = 0; k < n + 10; k++) { //현재 마름모 크기
+        
+        for(int k = 0; k <= n; k++) { //현재 마름모 크기
             int cost = k * k + (k+1) * (k+1); //현재 마름모 격자의 비용
             for(int i = 0; i < n; i++) {
                 for(int j = 0; j < n; j++) {
                     cnt = 0;
-                    visited = new boolean[n][n];
-                    dfs(i,j,0,k);
-                    maxCnt = Math.max(cnt,maxCnt);
+                    
+                    for(int r = 0; r < n; r++) {
+                        for(int c = 0; c < n; c++) {
+                            if(grid[i][j] == 1 && Math.abs(i - r) + Math.abs(j -c) <= k){
+                                //마름모 내부라면
+                                cnt++;
+                            }
+                        }
+                    }
+                if(cost <= m * cnt) maxCnt = Math.max(cnt,maxCnt);
                 }
             }
-            if(cost > maxCnt * m) {
-                // 손해가 발생하는 경우
-                continue;
-
-            }
-            preMaxCnt = maxCnt;
         }
-        System.out.println(preMaxCnt);
+        System.out.println(maxCnt);
 
-    }
-
-    public static void dfs(int row, int col, int currentDepth,int maxDepth) {
-        if(currentDepth == maxDepth+1) {
-            return;
-        }
-        visited[row][col] = true;
-
-        if(grid[row][col] == 1) cnt++;
-
-        int[] drs = {1,0,-1,0};
-        int[] dcs = {0,1,0,-1};
-
-        for(int i = 0; i < 4; i++) {
-            //현재 위치에서 상하좌우로 움직이기.
-            int nextRow = row + drs[i];
-            int nextCol = col + dcs[i];
-            if(canMine(nextRow,nextCol)) {
-                dfs(nextRow,nextCol,currentDepth+1,maxDepth);
-            }
-        }
-    }
-
-    public static boolean inRange(int row, int col) {
-        return row >= 0 && row < n && col >= 0 && col < n;
-    }
-
-    public static boolean canMine(int row, int col) {
-        if(!inRange(row,col)) return false;
-        if(visited[row][col]) return false;
-        return true;
     }
 }
