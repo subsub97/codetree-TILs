8423bcddcc31384cce08b9892eaccc627910c0f4
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-02-01 17:00:13 +0900

    Title: 기울어진 직사각형, Time: 102ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240201/기울어진 직사각형/slanted-rectangle.java
+++ 240201/기울어진 직사각형/slanted-rectangle.java
@@ -21,14 +21,12 @@ public class Main {
         int ans = 0;
         for(int i = 1; i < n; i++) {
             for(int j = 1; j < n-1; j++) {
-                for (int k = 1; k < n; k++) {
                     if(isStartSpot(i,j)) {
-                        currSum = sumSquare(i,j,k);
+                        currSum = sumSquare(i,j);
                         ans = Math.max(ans, currSum);
                     }
                 }
             }
-        }
         System.out.print(ans);
     }
 
@@ -41,28 +39,48 @@ public class Main {
         return row >= 0 && row < n && col >= 0 && col < n;
     }
 
-    public static int sumSquare(int row, int col,int k) {
+    public static int sumSquare(int row, int col) {
         // 기울어진 사각형 모양의 숫자들을 합산하는 함수
-        int[] drs = {-1,-1,1,1};
-        int[] dcs = {1,-1,-1,1};
+        int[] drs = {0,-1,-1,1,1};
+        int[] dcs = {0,1,-1,-1,1};
         int dirIdx = 0;
         int sumNumber = 0;
 
         int currRow = row;
         int currCol = col;
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < k; j++) {
-                currRow = currRow + drs[dirIdx];
-                currCol = currCol + dcs[dirIdx];
-                if(!inRange(currRow,currCol)) {
-                    return 0;
-                }
-                sumNumber += grid[currCol][currRow];
-            }
-            dirIdx = (dirIdx + 1) % 4;
+        while(dirIdx != 4 || (row != currRow && col != currCol)) {
+            dirIdx = selectDir(dirIdx,currRow,currCol,row,col);
+            currRow = currRow + drs[dirIdx];
+            currCol = currCol + dcs[dirIdx];
+
+            sumNumber += grid[currRow][currCol];
         }
+
 
         return sumNumber;
     }
+
+    public static int selectDir(int dir, int row, int col,int startRow,int startCol) {
+        if(dir == 0) {
+            return 1;
+        }
+        if(dir == 1) {
+            if(row == 1 || col == n-1) return 2;
+        }
+        if(dir == 2) {
+            if(row == 0 || col == 1 || col == startRow)
+                return 3;
+        }
+        if(dir == 3) {
+            if(row == startRow -1 || col == 0)
+                return 4;
+        }
+        return dir;
+    }
 }
+/*
+우측 대각으로 올라가는건 row 1 까지고 col이 < n인 경우 가능
+좌측 대각으로 올라가는건 row 0 까지고 col >=0 경우 가능
+좌측 하단으로 내려오는건 출발한 row-1 까지가능
+ */
