8558fadcaf66c5cb43ff6d66403b19d1765d1ff4
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-02-01 23:09:24 +0900

    Title: 사각형 채우기 3, Time: 125ms, Memory: 9MB, Status: Passed - CodeTree

--- 240201/사각형 채우기 3/rectangle-fill-3.java
+++ 240201/사각형 채우기 3/rectangle-fill-3.java
@@ -7,7 +7,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine().trim());
-        int[] dp = new int[1001];
+        long[] dp = new long[1001];
 
         dp[0] = 1;
         dp[1] = 2;
@@ -15,15 +15,16 @@ public class Main {
         dp[3] = 22;
 
         for (int i = 4; i <= n; i++) {
-            int calcValue = 0;
+            long calcValue = 0;
             for (int j = i; j > 0; j--) {
                 if(i - j == 2){
-                    calcValue += dp[j] * 3;
+                    calcValue = (calcValue +(dp[j] * 3)) % 1_000_000_007;
                     continue;
                 }
-                calcValue += dp[j] * 2;
+                calcValue = (calcValue % 1_000_000_007 + (dp[j] * 2) % 1_000_000_007) % 1_000_000_007;
+                
             }
-            dp[i] = (calcValue + 2) % 1_000_000_007;
+            dp[i] = (calcValue % 1_000_000_007 + 2 % 1_000_000_007) % 1_000_000_007;
         }
 
         System.out.println(dp[n]);
