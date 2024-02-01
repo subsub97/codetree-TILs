54adaa34c08b4f3ac238967818649085da1ae88f
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-02-02 00:50:26 +0900

    Title: 사각형 채우기 2, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

--- 240202/사각형 채우기 2/rectangle-fill-2.java
+++ 240202/사각형 채우기 2/rectangle-fill-2.java
@@ -14,7 +14,7 @@ public class Main {
         dp[2] = 3;
 
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2] *2;
+            dp[i] = (dp[i-1] + dp[i-2] *2) % 10_007;
         }
 
         System.out.println(dp[n]);
