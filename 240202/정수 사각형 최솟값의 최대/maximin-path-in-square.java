05ecfc3d6568a201ef91fb6a2a1ce4325a067186
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-02-02 04:11:03 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 159ms, Memory: 12MB, Status: Passed - CodeTree

--- 240202/정수 사각형 최솟값의 최대/maximin-path-in-square.java
+++ 240202/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -2,8 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -18,29 +16,18 @@ public class Main {
             }
         }
 
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = Math.min(dp[0][i - 1], grid[0][i]);
+            dp[i][0] = Math.min(dp[i - 1][0], grid[i][0]);
+        }
 
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if(i == 0 && j ==0) {
-                    dp[i][j] = grid[i][j];
-                    continue;
-                }
-                if(i == 0){
-                    dp[i][j] = Math.min(dp[i][j - 1], grid[i][j]);
-                    continue;
-                }
-                if(j == 0) {
-                    dp[i][j] = Math.min(dp[i - 1][j], grid[i][j]);
-                    continue;
-                }
-                else{
-                    dp[i][j] = Math.min(Math.max(dp[i - 1][j], dp[i][j - 1]), grid[i][j]);
-                }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(Math.max(dp[i - 1][j], dp[i][j - 1]), grid[i][j]);
             }
         }
 
         System.out.println(dp[n-1][n-1]);
-
     }
 }
