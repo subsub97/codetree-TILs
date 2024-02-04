29fbf7b16fca37e5677435210a05fc06a3aee6e4
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-02-05 01:46:34 +0900

    Title: 컨베이어 벨트, Time: 128ms, Memory: 9MB, Status: Passed - CodeTree

--- 240205/컨베이어 벨트/conveyor-belt.java
+++ 240205/컨베이어 벨트/conveyor-belt.java
@@ -1,8 +1,11 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -31,13 +34,17 @@ public class Main {
             q1.addFirst(q2.pollLast());
         }
 
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         for(Integer num:q1) {
-            System.out.print(num +" ");
+            bw.append(num +" ");
         }
 
-        System.out.println();
+        bw.append("\n");
         for(Integer num:q2) {
-            System.out.print(num +" ");
+            bw.append(num +" ");
         }
+
+        bw.close();
     }
 }
