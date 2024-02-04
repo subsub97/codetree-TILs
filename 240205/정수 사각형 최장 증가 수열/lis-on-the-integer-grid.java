e7fd28cbd4df1c580823e54389963ffe45afc4c1
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-02-05 01:10:50 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1523ms, Memory: 85MB, Status: Passed - CodeTree

--- 240205/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 240205/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -1,83 +1,84 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.Arrays;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Cell implements Comparable<Cell> {
+    int num, x, y;
+
+    public Cell(int num, int x, int y) {
+        this.num = num;
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Cell c) {
+        return this.num - c.num;      // num 기준으로 오름차순 정렬합니다.
+    }
+}
 
 public class Main {
+    public static final int DIR_NUM = 4;
+    public static final int MAX_N = 500;
+    
+    // 변수 선언
     public static int n;
-    public static int[][] grid;
-    public static int[][] dp;
-    public static Cell[] cells;
+    public static int[][] grid = new int[MAX_N][MAX_N];
+    public static int[][] dp = new int[MAX_N][MAX_N];
+    
+    public static ArrayList<Cell> cells = new ArrayList<>();
+    public static int ans = 0;
+    
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        n = sc.nextInt();
 
-        n = Integer.parseInt(br.readLine().trim());
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
 
-        grid = new int[n][n];
-        dp = new int[n][n];
-        cells = new Cell[n*n];
-        int idx = 0;
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                int num = Integer.parseInt(st.nextToken());
-                cells[idx] = new Cell(num, j, i);
-                grid[i][j] = num;
-                idx++;
+        // 각 칸에 적혀있는 정수값 기준으로
+        // 오름차순이 되도록 칸의 위치들을 정렬합니다.
+        // 편하게 정렬하기 위해
+        // (칸에 적혀있는 값, 행 위치, 열 위치) 순으로 넣어줍니다.
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                cells.add(new Cell(grid[i][j], i, j));
+        
+        // 오름차순으로 정렬을 진행합니다.
+        Collections.sort(cells);
+
+        // 처음 DP 값들은 전부 1로 초기화해줍니다. (해당 칸에서 시작하는 경우)
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
                 dp[i][j] = 1;
+
+        // 정수값이 작은 칸부터 순서대로 보며
+        // 4방향에 대해 dp 값을 갱신해줍니다.
+        for(int i = 0; i < cells.size(); i++) {
+            int x = cells.get(i).x;
+            int y = cells.get(i).y;
+            int[] dx = new int[]{-1, 1,  0, 0};
+            int[] dy = new int[]{ 0, 0, -1, 1};
+
+            // 인접한 4개의 칸에 대해 갱신을 진행합니다.
+            for(int j = 0; j < DIR_NUM; j++) {
+                int nx = x + dx[j], ny = y + dy[j];
+                if(inRange(nx, ny) && grid[nx][ny] > grid[x][y])
+                    dp[nx][ny] = Math.max(dp[nx][ny], dp[x][y] + 1);
             }
         }
 
-        Arrays.sort(cells);
+        // 전체 수들 중 최댓값을 찾습니다.
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                ans = Math.max(ans, dp[i][j]);
 
-        int ans = 0;
-
-        for (Cell cell : cells) {
-            ans = checkCnt(cell.x, cell.y);
-        }
-
-        System.out.println(ans);
-
-    }
-
-    public static int checkCnt(int x, int y) {
-        int[] dxs = {1, -1, 0, 0};
-        int[] dys = {0, 0, 1, -1};
-        int dir = 0;
-
-        for (int i = 0; i < 4; i++) {
-            int nextX = x + dxs[dir];
-            int nextY = y + dys[dir];
-
-            if(inRange(nextY,nextX)) {
-                if(grid[y][x] > grid[nextY][nextX]) {
-                    dp[y][x] = dp[nextY][nextX] + 1;
-                }
-            }
-            dir++;
-        }
-
-        return dp[y][x];
-    }
-
-    public static boolean inRange(int row,int col) {
-        return row >= 0 && row < n && col >= 0 && col < n;
-    }
-
-    public static class Cell implements Comparable<Cell> {
-        int num,x,y;
-
-        Cell(int num, int x, int y) {
-            this.num = num;
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public int compareTo(Cell c) {
-            return this.num - c.num;
-        }
+        System.out.print(ans);
     }
 }
