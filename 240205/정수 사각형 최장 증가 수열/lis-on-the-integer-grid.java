33452780136d45298087c5d9001bc8fa578c282d
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-02-05 01:10:25 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 782ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240205/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 240205/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -2,11 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
     public static int n;
     public static int[][] grid;
     public static int[][] dp;
+    public static Cell[] cells;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,21 +17,25 @@ public class Main {
 
         grid = new int[n][n];
         dp = new int[n][n];
-
+        cells = new Cell[n*n];
+        int idx = 0;
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                grid[i][j] = Integer.parseInt(st.nextToken());
+                int num = Integer.parseInt(st.nextToken());
+                cells[idx] = new Cell(num, j, i);
+                grid[i][j] = num;
+                idx++;
                 dp[i][j] = 1;
             }
         }
 
+        Arrays.sort(cells);
+
         int ans = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                ans = Math.max(ans,checkCnt(i,j));
-            }
+        for (Cell cell : cells) {
+            ans = checkCnt(cell.x, cell.y);
         }
 
         System.out.println(ans);
@@ -58,5 +64,20 @@ public class Main {
 
     public static boolean inRange(int row,int col) {
         return row >= 0 && row < n && col >= 0 && col < n;
+    }
+
+    public static class Cell implements Comparable<Cell> {
+        int num,x,y;
+
+        Cell(int num, int x, int y) {
+            this.num = num;
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Cell c) {
+            return this.num - c.num;
+        }
     }
 }
