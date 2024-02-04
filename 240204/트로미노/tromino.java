13aa6a0391be92e8a31c67f6130f1b9e0cc25bed
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-02-04 23:36:09 +0900

    Title: 트로미노, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240204/트로미노/tromino.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static final int MAX_DIR = 3;
+    public static int n;
+    public static int m;
+    public static int[][] grid;
+    public static boolean[][] visited;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        grid = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                visited = new boolean[n][m];
+                ans = Math.max(ans,findMaxValue(i, j,1,0));
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    public static int findMaxValue(int row, int col, int depth,int value) {
+        visited[row][col] = true;
+        if(depth == 3) {
+            visited[row][col] = false;
+            return value += grid[row][col];
+        }
+
+        int[] drs = {1,0,-1};
+        int[] dcs = {0,1,0};
+        int dir = 0;
+
+        int maxValue = 0;
+
+        for (int i = 0; i < MAX_DIR; i++) {
+            int nextRow = row + drs[dir];
+            int nextCol = col + dcs[dir];
+            if(canGo(nextRow,nextCol)) {
+                //갈 수 있는 곳인 경우
+                maxValue = Math.max(findMaxValue(nextRow,nextCol, depth + 1,
+                        value+grid[row][col]),maxValue);
+
+            }
+            dir++;
+        }
+        visited[row][col] = false;
+        return maxValue;
+    }
+
+    public static boolean inRange(int row, int col) {
+        return row >= 0 && row < n && col >= 0 && col < n;
+    }
+
+    public static boolean canGo(int row, int col) {
+        if(!inRange(row,col)) {
+            return false;
+        }
+        if(visited[row][col]) {
+            return false;
+        }
+        return true;
+    }
+
+}
