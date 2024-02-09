38aa5fe700c093e14875f9e5b6d8eb97202f6a36
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-02-09 17:08:15 +0900

    Title: 비를 피하기, Time: 102ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240209/비를 피하기/stay-out-of-rain.java
@@ -0,0 +1,102 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int n,h,m;
+    public static int[][] grid;
+    public static int[][] ans;
+    public static boolean[][] visited;
+    public static ArrayList<Pair> locations = new ArrayList<>();
+    public static Queue<Pair> q = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        h = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        grid = new int[n][n];
+        ans = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+                if (grid[i][j] == 2) {
+                    locations.add(new Pair(i, j));
+                }
+            }
+        }
+
+        for(Pair p : locations) {
+            visited = new boolean[n][n];
+            visited[p.row][p.col] = true;
+            q.add(p);
+            bfs();
+            if (ans[p.row][p.col] == 0) {
+                ans[p.row][p.col] = -1;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static boolean inRange(int r, int c) {
+        return r >= 0 && r < n && c >= 0 && c < n;
+    }
+    public static boolean canGo(int r, int c) {
+        if (!inRange(r, c)) {
+            return false;
+        }
+        if(visited[r][c] || grid[r][c] == 1) return false;
+        return true;
+    }
+    public static void bfs() {
+        int[] drs = {1, -1, 0, 0};
+        int[] dcs = {0, 0, 1, -1};
+        int cnt = 0;
+
+        Pair startP = q.peek();
+
+        while (!q.isEmpty()) {
+            Pair p = q.poll();
+
+            int curR = p.row;
+            int curC = p.col;
+
+
+            if(grid[curR][curC] == 3) {
+                //비를 피하는 곳이라면
+                ans[startP.row][startP.col] = cnt;
+                break;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nR = curR + drs[i];
+                int nC = curC + dcs[i];
+                if (canGo(nR, nC)) {
+                    cnt++;
+                    q.add(new Pair(nR, nC));
+                    visited[nR][nC] = true;
+                }
+                }
+            }
+        }
+
+
+
+    public static class Pair {
+        int row, col;
+        Pair(int r, int c) {
+            row = r;
+            col = c;
+        }
+    }
+
+}
