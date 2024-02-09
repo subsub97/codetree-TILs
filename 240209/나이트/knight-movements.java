2b2c1a9f0b1158c43cd7faf02478338c569f9503
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-02-09 16:30:49 +0900

    Title: 나이트, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240209/나이트/knight-movements.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int n,r2,c2,ans;
+    public static int[][] grid;
+    public static boolean[][] visited;
+    public static Queue<Pair> q = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int r1,c1;
+        n = Integer.parseInt(st.nextToken());
+        ans = -1;
+
+        st = new StringTokenizer(br.readLine());
+        r1 = Integer.parseInt(st.nextToken()) -1;
+        c1 = Integer.parseInt(st.nextToken()) -1;
+        r2 = Integer.parseInt(st.nextToken())-1;
+        c2 = Integer.parseInt(st.nextToken())-1;
+
+        visited = new boolean[n][n];
+
+        q.add(new Pair(r1, c1, 0));
+        bfs();
+        System.out.println(ans);
+
+    }
+    public static boolean inRange(int r, int c) {
+        return r >= 0 && r < n && c >= 0 && c < n;
+    }
+    public static boolean canGo(int r, int c) {
+        if (!inRange(r, c)) {
+            return false;
+        }
+        if(visited[r][c]) return false;
+        return true;
+    }
+    public static void bfs() {
+        int[] drs = {2,1,2,-1,-1,-2,-2,1};
+        int[] dcs = {1,-2,-1,-2,2,-1,1,2};
+
+        while (!q.isEmpty()) {
+            Pair p = q.poll();
+
+            int curR = p.row;
+            int curC = p.col;
+
+            for (int i = 0; i < 8; i++) {
+                int nR = curR + drs[i];
+                int nC = curC + dcs[i];
+                if(canGo(nR,nC)){
+                    if(nR == r2 && nC == c2) {
+                        ans = p.distance + 1;
+                        while(!q.isEmpty()) {
+                            q.poll();
+                        }
+                        break;
+                    }
+                    visited[nR][nC] = true;
+                    q.add(new Pair(nR, nC, p.distance + 1));
+                }
+            }
+        }
+
+    }
+
+    public static class Pair {
+        int row, col,distance;
+        Pair(int r, int c,int d) {
+            row = r;
+            col = c;
+            distance = d;
+        }
+    }
+
+}
