437f7b229c23a0190e47a7fb214145f15e481e25
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-02-18 21:46:07 +0900

    Title: 천 개의 정거장, Time: 863ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240218/천 개의 정거장/thousand-stops.java
+++ 240218/천 개의 정거장/thousand-stops.java
@@ -9,6 +9,20 @@ import java.util.StringTokenizer;
 public class Main {
     public static final int MAX_STATION = 1000;
 
+    public static class Element implements Comparable<Element> {
+        int index;
+        int dist;
+        Element(int d, int i) {
+            dist = d;
+            index = i;
+        }
+
+        @Override
+        public int compareTo(Element e) {
+            return this.dist - e.dist;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         //최소 비용과 최소 시간 구하기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,7 +32,11 @@ public class Main {
         int endB = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
 
-        int[][] graph = new int[MAX_STATION + 1][MAX_STATION + 1];
+        ArrayList<Element>[] graph = new ArrayList[MAX_STATION + 1];
+
+        for (int i = 1; i <= MAX_STATION; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
         for (int i = 1; i <= n; i++) {
             //1번 버스부터 n번 버스까지 노선 추가
@@ -36,55 +54,57 @@ public class Main {
             for (int j = 0; j < stationCount-1; j++) {
                 //버스 노선별 연결된 정거장 추가
                 for (int k = j+1; k < stationCount; k++) {
-                    graph[stationsInfo[j]][stationsInfo[k]] = fare;
+                    graph[stationsInfo[j]].add(new Element(fare, stationsInfo[k]));
                 }
+
             }
         }
 
-        long[] dist = new long[MAX_STATION + 1];
+        int[] dist = new int[MAX_STATION + 1];
         int[] path = new int[MAX_STATION + 1];
 
         for (int i = 0; i <= MAX_STATION ; i++) {
-            dist[i] = 20_000_000_000L;
+            dist[i] = 2_000_000_000;
         }
-        dist[startA] = 0; //시작지점 비용 0으로
-        boolean[] visited = new boolean[MAX_STATION + 1];
+        dist[startA] = 0; //시작지점 비용 0으로 선언
 
-        for (int i = 1; i <= MAX_STATION; i++) {
-            int minIndex = -1;
-            for (int j = 1; j <= MAX_STATION ; j++) {
-                if(visited[j]) continue;
+        PriorityQueue<Element> pq = new PriorityQueue<>();
+        pq.add(new Element(0, startA));
 
-                if(minIndex == -1 || dist[minIndex] > dist[j]){
-                    minIndex = j;
+        while(!pq.isEmpty()) {
+            int minIndex = pq.peek().index;
+            int minDist = pq.peek().dist;
+            pq.poll();
+
+            if (minDist != dist[minIndex])
+                continue;
+
+            for (int i = 0; i < graph[minIndex].size(); i++) {
+                //다익스트라 ㄱ
+                int targetDist = graph[minIndex].get(i).dist;
+                int targetIndex = graph[minIndex].get(i).index;
+                int newDist = minDist + targetDist;
+                if (dist[targetIndex] > newDist) {
+                    dist[targetIndex] = newDist;
+                    path[targetIndex] = minIndex;
+                    pq.add(new Element(newDist, targetIndex));
                 }
             }
-
-            visited[minIndex] = true;
-
-            for (int j = 1; j <= MAX_STATION ; j++) {
-                if(graph[minIndex][j] == 0) continue;
-
-                if(dist[j] > dist[minIndex] + graph[minIndex][j]) {
-                    dist[j] = dist[minIndex] + graph[minIndex][j];
-                    path[j] = minIndex;
-                }
-            }
         }
-            // 도착점부터 시작점으로 역 순회하면서 최단 경로 찾기
-            int x = endB;
-            Stack<Integer> minPaths = new Stack<>();
-            int minCnt = 1;
+        // 도착점부터 시작점으로 역 순회하면서 최단 경로 찾기
+        int x = endB;
+        Stack<Integer> minPaths = new Stack<>();
+        int minCnt = 1;
 
-            if(dist[endB] == 20_000_000_000L) {
-                System.out.println("-1 -1");
-            }
+        if(dist[endB] == 2_000_000_000) {
+            System.out.println("-1 -1");
+        }
 
-            while(x != startA){
-                minPaths.push(x);
-                minCnt++;
-                x = path[x];
-            }
+        while(x != startA){
+            minPaths.push(x);
+            minCnt++;
+            x = path[x];
+        }
 
         System.out.print(dist[endB] + " ");
         System.out.println(minCnt);
