dc430151ff695b98b238d9b1c96e6c96883126a4
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-02-18 21:38:50 +0900

    Title: 천 개의 정거장, Time: 842ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240218/천 개의 정거장/thousand-stops.java
+++ 240218/천 개의 정거장/thousand-stops.java
@@ -9,20 +9,6 @@ import java.util.StringTokenizer;
 public class Main {
     public static final int MAX_STATION = 1000;
 
-    public static class Element implements Comparable<Element> {
-        int index;
-        int dist;
-        Element(int d, int i) {
-            dist = d;
-            index = i;
-        }
-
-        @Override
-        public int compareTo(Element e) {
-            return this.dist - e.dist;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         //최소 비용과 최소 시간 구하기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,11 +18,7 @@ public class Main {
         int endB = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
 
-        ArrayList<Element>[] graph = new ArrayList[MAX_STATION + 1];
-
-        for (int i = 1; i <= MAX_STATION; i++) {
-            graph[i] = new ArrayList<>();
-        }
+        int[][] graph = new int[MAX_STATION + 1][MAX_STATION + 1];
 
         for (int i = 1; i <= n; i++) {
             //1번 버스부터 n번 버스까지 노선 추가
@@ -54,40 +36,38 @@ public class Main {
             for (int j = 0; j < stationCount-1; j++) {
                 //버스 노선별 연결된 정거장 추가
                 for (int k = j+1; k < stationCount; k++) {
-                    graph[stationsInfo[j]].add(new Element(fare, stationsInfo[k]));
+                    graph[stationsInfo[j]][stationsInfo[k]] = fare;
                 }
-
             }
         }
 
-        int[] dist = new int[MAX_STATION + 1];
+        long[] dist = new long[MAX_STATION + 1];
         int[] path = new int[MAX_STATION + 1];
 
         for (int i = 0; i <= MAX_STATION ; i++) {
-            dist[i] = 2_000_000_000;
+            dist[i] = 20_000_000_000L;
         }
-        dist[startA] = 0; //시작지점 비용 0으로 선언
+        dist[startA] = 0; //시작지점 비용 0으로
+        boolean[] visited = new boolean[MAX_STATION + 1];
 
-        PriorityQueue<Element> pq = new PriorityQueue<>();
-        pq.add(new Element(0, startA));
+        for (int i = 1; i <= MAX_STATION; i++) {
+            int minIndex = -1;
+            for (int j = 1; j <= MAX_STATION ; j++) {
+                if(visited[j]) continue;
 
-        while(!pq.isEmpty()) {
-            int minIndex = pq.peek().index;
-            int minDist = pq.peek().dist;
-            pq.poll();
+                if(minIndex == -1 || dist[minIndex] > dist[j]){
+                    minIndex = j;
+                }
+            }
 
-            if (minDist != dist[minIndex])
-                continue;
+            visited[minIndex] = true;
 
-            for (int i = 0; i < graph[minIndex].size(); i++) {
-                //다익스트라 ㄱ
-                int targetDist = graph[minIndex].get(i).dist;
-                int targetIndex = graph[minIndex].get(i).index;
-                int newDist = minDist + targetDist;
-                if (dist[targetIndex] > newDist) {
-                    dist[targetIndex] = newDist;
-                    path[targetIndex] = minIndex;
-                    pq.add(new Element(newDist, targetIndex));
+            for (int j = 1; j <= MAX_STATION ; j++) {
+                if(graph[minIndex][j] == 0) continue;
+
+                if(dist[j] > dist[minIndex] + graph[minIndex][j]) {
+                    dist[j] = dist[minIndex] + graph[minIndex][j];
+                    path[j] = minIndex;
                 }
             }
         }
