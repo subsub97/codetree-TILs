97bac04635e0c6786523147e846473437413d5f6
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-02-19 14:29:26 +0900

    Title: 아스키코드표 맞추기, Time: 101ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240219/아스키코드표 맞추기/chart-of-ASCII.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        while(st.hasMoreTokens()){
+            System.out.print((char)Integer.parseInt(st.nextToken()) + " ");    
+        }
+        
+    }
+}
