2228422128073a00f8838c95d58d8e87a87f8138
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-02-21 12:56:19 +0900

    Title: 대응되는 수와 문자, Time: 2199ms, Memory: 62MB, Status: Passed - CodeTree

--- 240221/대응되는 수와 문자/corresponding-numbers-and-characters.java
+++ 240221/대응되는 수와 문자/corresponding-numbers-and-characters.java
@@ -1,31 +1,37 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.HashMap;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n, m;
+    public static String[] words = new String[MAX_N + 1];
+    public static HashMap<String, Integer> StringToNum = new HashMap<>();
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        HashMap<String, Integer> stringKeyMap = new HashMap<>();
-        HashMap<Integer, String> intKeyMap =  new HashMap<>();
-
-        for(int i = 0; i < n; i++) {
-            String s = br.readLine().trim();
-            stringKeyMap.put(s, i + 1);
-            intKeyMap.put(i+1, s);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        m = sc.nextInt();
+        
+        // 각 숫자의 대응되는 문자를 array에,
+        // 각 문자의 대응되는 숫자를 hashmap에 기록해줍니다.
+        for(int i = 1; i <= n; i++) {
+            words[i] = sc.next();
+            StringToNum.put(words[i], i);
         }
 
-        for(int i = 0; i < m; i++) {
-            String s = br.readLine().trim();
-            if(stringKeyMap.containsKey(s)) {
-                System.out.println(stringKeyMap.get(s));
-            }
-            else{
-                System.out.println(intKeyMap.get(Integer.parseInt(s)));
-            }
+        while(m-- > 0) {
+            String key = sc.next();
+
+            // 입력된 값이 숫자일 때에는 array에 저장한 문자를 출력합니다.
+            if('0' <= key.charAt(0) && key.charAt(0) <= '9')
+                System.out.println(words[Integer.parseInt(key)]);
+            
+            // 입력된 값이 문자일 때에는 hashmap에 기록된 대응되는 숫자를 출력합니다.
+            else
+                System.out.println(StringToNum.get(key));
         }
     }
 }
