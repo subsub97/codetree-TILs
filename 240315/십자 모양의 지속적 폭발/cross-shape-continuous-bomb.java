4055dac431eea3e1f6d9733ce32d632a0bde44d2
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-03-15 18:23:17 +0900

    Title: 십자 모양의 지속적 폭발, Time: 822ms, Memory: 28MB, Status: Passed - CodeTree

--- 240315/십자 모양의 지속적 폭발/cross-shape-continuous-bomb.java
+++ 240315/십자 모양의 지속적 폭발/cross-shape-continuous-bomb.java
@@ -1,118 +1,84 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    public static int n,m;
-    public static int[][] grid;
-    public static boolean[] exploded;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        grid = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                grid[i][j] = Integer.parseInt(st.nextToken());
+    public static final int OUT_OF_GRID = -1;
+    public static final int MAX_N = 200;
+    
+    public static int n, m;
+    public static int[][] grid = new int[MAX_N][MAX_N];
+    public static int[][] nextGrid = new int[MAX_N][MAX_N];
+    
+    public static boolean inBombRange(int x, int y, int centerX, int centerY, int bombRange) {
+        return (x == centerX || y == centerY) && 
+               Math.abs(x - centerX) + Math.abs(y - centerY) < bombRange;
+    }
+    
+    public static void bomb(int centerX, int centerY) {
+        // Step1. nextGrid 값을 0으로 초기화합니다.
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                nextGrid[i][j] = 0;
+    
+        // Step2. 폭탄이 터질 위치는 0으로 채워줍니다.
+        int bombRange = grid[centerX][centerY];
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                if(inBombRange(i, j, centerX, centerY, bombRange))
+                    grid[i][j] = 0;
+        
+        // Step3. 폭탄이 터진 이후의 결과를 nextGrid에 저장합니다.
+        for(int j = 0; j < n; j++) {
+            int nextRow = n - 1;
+            for(int i = n - 1; i >= 0; i--) {
+                if(grid[i][j] > 0)
+                    nextGrid[nextRow--][j] = grid[i][j];
             }
         }
+        
+        // Step4. grid로 다시 값을 옮겨줍니다.
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = nextGrid[i][j];
+    }
+    
+    // 해당 col 열에 폭탄이 터질 위치를 구합니다.
+    // 없다면 OUT_OF_GRID를 반환합니다.
+    public static int getBombRow(int col) {
+        for(int row = 0; row < n; row++)
+            if(grid[row][col] != 0)
+                return row;
+        
+        return OUT_OF_GRID;
+    }
 
-        for (int i = 0; i < m; i++) {
-            int col = Integer.parseInt(br.readLine().trim()) -1;
-            //M번의 열이 선택되고 폭탄이 터진다.
-            exploded = new boolean[n];
-            simulate(col);
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+        
+        // m번에 걸쳐 폭탄이 터집니다.
+        while(m-- > 0) {
+            int bombCol = sc.nextInt();
+            bombCol--;
+            
+            // 폭탄이 터지게 될 위치를 구합니다.
+            int bombRow = getBombRow(bombCol);
+            
+            if(bombRow == OUT_OF_GRID)
+                continue;
+            
+            bomb(bombRow, bombCol);
         }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++)
                 System.out.print(grid[i][j] + " ");
-            }
             System.out.println();
         }
-
     }
-
-    public static void simulate(int col) {
-        int row = findStartRow(col);
-        if(row == -1) return;
-
-        explode(row, col);
-        drop();
-    }
-
-    public static int findStartRow(int c) {
-        int r = -1;
-
-        for(int i = 0; i  < n; i++) {
-            if(grid[i][c] != 0) {
-                r = i;
-                return r;
-            }
-        }
-        return r;
-    }
-
-    public static boolean inRange(int r, int c) {
-        return r >=0 && r < n && c >= 0 && c < n;
-    }
-
-    public static void explode(int r, int c) {
-        int[] drs = {1, -1, 0, 0};
-        int[] dcs = {0, 0, -1, 1};
-
-        int bombPower = grid[r][c] - 1;
-        grid[r][c] = 0;
-        exploded[c] = true;
-
-        for (int i = 0; i < 4; i++) {
-            for (int j = 1; j <= bombPower; j++) {
-                int nR = r + (drs[i] * j);
-                int nC = c + (dcs[i] * j);
-
-                if(inRange(nR,nC)){
-                    //폭발
-                    exploded[nC] = true;
-                    grid[nR][nC] = 0;
-                }
-            }
-        }
-    }
-
-    public static void drop() {
-        //중력의 영향을 받아야 하는 경우라면 적용하기
-        for(int i = 0 ; i < n; i++) {
-            if(exploded[i]) {
-                //폭발한 숫자가 있기에 중력 작용시키기
-                int[] temp = new int[n];
-                int tempIdx = 0;
-
-                for (int j = n-1; j >= 0; j--) {
-                    if(grid[j][i] != 0) {
-                        temp[tempIdx++] = grid[j][i];
-                    }
-                }
-                int idx = 0;
-
-                for (int j = n-1; j >= 0; j--) {
-                    grid[j][i] = temp[idx++];
-                }
-            }
-        }
-    }
-
-    /*
-    1. Col이 주어지면 유효한 Row 찾기
-    2. 십자로 터트리기
-        터트리면서 exploded로 터졌는지 확인하고 터진 열이라면 중력 적용하기
-    3. 중력으로 떨구기
-     */
 }
