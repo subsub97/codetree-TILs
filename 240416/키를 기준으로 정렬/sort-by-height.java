db191eecde04ee295ea353c55289a26e36eda7d7
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-04-16 13:59:55 +0900

    Title: 키를 기준으로 정렬, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 240416/키를 기준으로 정렬/sort-by-height.java
+++ 240416/키를 기준으로 정렬/sort-by-height.java
@@ -1,45 +1,47 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
+import java.util.Arrays;
 
-    class Student{
-        public String name;
-        public int weight;
-        public int height;
+// 학생들의 정보를 나타내는 클래스 선언
+class Student implements Comparable<Student> {
+    String name;
+    int height, weight;
 
-        Student(String name, int height, int weight) {
-            this.name = name;
-            this.height = height;
-            this.weight = weight;
-        }
+    public Student(String name, int height, int weight){
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
 
-    };
+    @Override
+    public int compareTo(Student student) { // 키를 기준 오름차순 정렬합니다.
+        return this.height - student.height;
+    }
+};
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        // 변수 선언 및 입력:
+        int n = sc.nextInt();
         Student[] students = new Student[n];
-
         for(int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = sc.next();
+            int height = sc.nextInt();
+            int weight = sc.nextInt();
 
-            String name = st.nextToken();
-            int height = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            students[i] = new Student(name,height,weight);
+            // Student 객체를 생성해 리스트에 추가합니다.
+            students[i] = new Student(name, height, weight);
         }
 
-        // Arrays.sort(students, (a,b) -> a.height - b.height);
-        Arrays.sort(students, new Comparator<Student>() {
-            @Override
-            public int compare(Student a, Student b) { // 키를 기준 오름차순 정렬합니다.
-                return a.height - b.height;
-            }
-        });
+        // custom comparator를 활용한 정렬
+        Arrays.sort(students);
 
-        for(Student s : students) {
-            System.out.println(s.name + " " + s.height + " " + s.weight);
+        // 결과를 출력합니다.
+        for (int i = 0; i < n; i++){
+            System.out.print(students[i].name + " ");
+            System.out.print(students[i].height + " ");
+            System.out.println(students[i].weight);
         }
     }
 }
