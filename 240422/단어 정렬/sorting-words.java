75ec8a68352f24d113a0aa3aebae1a04e264eddc
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-04-22 17:51:48 +0900

    Title: 단어 정렬, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240422/단어 정렬/sorting-words.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] words = new String[n];
+        for (int i=0; i<n; i++){
+            words[i] = sc.next();
+        }
+        Arrays.sort(words);
+        for(int i=0; i<n; i++){
+            System.out.println(words[i]);
+        }
+    }
+
+
+}
