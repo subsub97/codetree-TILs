01bd0ee0c99f1d1ea37860fac55f0e41d30c2a2e
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-04-23 18:57:59 +0900

    Title: 2개씩 그룹짓기, Time: 221ms, Memory: 14MB, Status: Passed - CodeTree

--- 240423/2개씩 그룹짓기/group-of-pairs.java
+++ 240423/2개씩 그룹짓기/group-of-pairs.java
@@ -3,13 +3,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int max = 0;
         int[] numbers = new int[n*2];
         for (int i=0; i<n*2; i++){
             numbers[i] = sc.nextInt();
-  
         }
         Arrays.sort(numbers);
-   
-        System.out.println(numbers[n-1]+numbers[n]);
+        for (int i=0; i<n; i++){
+            max = numbers[i]+numbers[numbers.length-i-1] > max ? numbers[i]+numbers[numbers.length-i-1] : max;
+        }
+        System.out.println(max);
     }
 }
