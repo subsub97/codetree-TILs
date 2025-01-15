7a165acd9c32c221dd0e592dee3ef37acc205b92
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2025-01-15 19:49:00 +0900

    Title: 가장 긴 접두사, Time: 159ms, Memory: 13MB, Status: Passed - CodeTree

--- 250115/가장 긴 접두사/longest-prefix.java
+++ 250115/가장 긴 접두사/longest-prefix.java
@@ -1,42 +1,73 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
-
-    static int[] table;
-    static String T;
-    static String rt;
-    static int max = 0;
-
-    public static void main(String[] args) throws Exception{
-        // Please write your code here.
-
+    
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        T = br.readLine();
-        table = new int[T.length()];
+        String T = br.readLine();  
+        String P;
 
-        rt = new StringBuilder(T).reverse().toString();
-        failureFunction();
-
-        System.out.println(max);
+        StringBuilder sb = new StringBuilder();
         
-    }
+        for(int i = T.length() - 1; i >= 0; i--) {
+            sb.append(T.charAt(i));
+        }
 
-    public static void failureFunction() {
-        
-        int pIdx = 0;
+        P = sb.toString();
 
-        char[] arr = T.toCharArray();
+        int maxSize = 100_000;
+        int[] f = new int[maxSize + 1];
+        f[0] = 0;
 
-        for(int idx = T.length() - 1; idx >= 0; idx--) {
-            while(pIdx != 0 && arr[idx] != arr[pIdx]) pIdx = table[pIdx - 1];
+        int n = T.length();
+        int m = P.length();
 
-            if(arr[idx] == arr[pIdx]) {
-                pIdx++;
-                table[idx] = pIdx;
-                max = Math.max(pIdx, max);
+        T = "#" + T;
+        P = "#" + P;
+
+        int jIdx = 1;
+
+        int j = 0;
+        for(int i = 1; i <= m; i++) {
+            //p 내부의 접두,접미 패턴을 찾는다.
+            
+  
+            // a b c a b c a a a
+            // a a a c b a c b a
+
+            //# a b a b a
+            //# a b a b a
+            while(j > 0 && T.charAt(j + 1) != P.charAt(i)) {
+                j = f[j-1];
+            }
+
+            if(T.charAt(j + 1) == P.charAt(i)) {
+                
+                f[i] = j + 1; // 0 , 
+                j = f[i];
             }
         }
+
+        // 한 문자씩 비교하며 패턴 문자열과 일치하게 되는 순간들 구하기
+        // int j = 0;
+        int ans = 0;
+        // for(int i = 1; i <= n; i++) {
+        //     while(j >= 0 && P.charAt(j + 1) != T.charAt(i)) {
+        //         j = f[j];
+        //     }
+        //     j++;
+
+        //     if(j == m) {
+        //         j = f[j];
+        //     }
+        // }
+
+        for(int i = 1; i <= m; i++) {
+            ans = Math.max(ans,f[i]);
+        }
+
+        System.out.println(ans);
+
     }
 }
