39f203ac77c5e47913c50885a2ea6c3e9a2bbc5a
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2025-02-18 19:39:55 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 115ms, Memory: 9MB, Status: Passed - CodeTree

--- 250218/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
+++ 250218/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
@@ -3,92 +3,71 @@ import java.io.*;
 
 public class Main {
     static int N;
-    static int maxLen = 0;
-    static ArrayList<Integer>[] lines = new ArrayList[1001];
-    static int[] selected;
-    static HashSet<Integer> set = new HashSet<>();
-    static int answer = 0;
-    
+    static ArrayList<Pair> p = new ArrayList<>();
+    static Pair[] selected;
+    static boolean[] used;
+    static int len;
+    static int[] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        
+
         for(int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            maxLen = Math.max(Math.max(a,maxLen),b);
-            
-            if(lines[a] == null) {
-                lines[a] = new ArrayList<>();
-                lines[a].add(b);
-                set.add(a);
+            p.add(new Pair(a,b));
+        }
+
+        p.sort(new Comparator<Pair>() {
+            @Override
+            public int compare(Pair p1, Pair p2) {
+                if (p1.s == p2.s) {
+                    return Integer.compare(p1.e, p2.e);
+                }
+                return Integer.compare(p1.s, p2.s);
             }
-            else {
-                lines[a].add(b);
+        });
+
+        used = new boolean[1001];
+        selected = new Pair[1001];
+
+        len = 0;
+
+        for(Pair p : p) {
+            if(used[p.s]) continue;
+            used[p.s] = true;
+            selected[len++] = p;
+        }
+
+        //LIS를 어떻게 체크하지?
+        // 앞에 있는 수직선의 끝보다 시작점이 큰 것만 골라야함
+        dp = new int[len];
+        Arrays.fill(dp, 1);
+        int ans = 1;
+        for(int i = 1; i < len; i++) {
+            for(int j = i; j >= 0; j--) {
+                if(selected[i].s > selected[j].e) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    ans = Math.max(ans, dp[i]);
+                }
             }
         }
 
-        selected = new int[set.size()];
-        
-        for(int i =0; i < 1001; i++) {
-        	if(lines[i] != null) {
-        		Collections.sort(lines[i]);
-        	}
-        }
-        
-        int idx = 0;
-        
-        for(int i = 1; i <= maxLen; i++) {
-        	if(lines[i] != null) {
-        		selected[idx++] = lines[i].get(0);
-        	}
-        }
-        
-        
-        System.out.print(lis());
-
+        System.out.println(ans);
     }
 
-    private static void dfs(int depth, int idx) {
-        if(depth == set.size()) {
-            //최장수열 체크
-            answer = Math.max(lis(),answer);
-            return;
-        }
+    private static class Pair {
+        int s;
+        int e;
 
-        for(int i = idx; i <= maxLen; i++) {
-            if(lines[i] == null) continue;
-            //있다면 최장수열 후보만들기
-            for(int j = 0; j < 1; j++) {
-                selected[depth] = lines[i].get(j);
-                dfs(depth + 1, i + 1);
-                selected[depth] = 0;
-            }
+        Pair(int s, int e) {
+            this.s = s;
+            this.e = e;
         }
-    }
-    
-    private static int lis() {
-    	int[] dp = new int[set.size()];
-    	int ans = 1;
-    	dp[0] = 1;
-    	
-    	for(int i = 1; i < set.size(); i++) {
-    		for(int j = i; j >= 0; j--) {
-    			if(j == i) {
-    				dp[i] = 1;
-    				continue;
-    			}
-    			if(selected[i] > selected[j]) {
-    				dp[i] = Math.max(dp[i],dp[j] + 1);
-    				ans = Math.max(ans, dp[i]);
-    			}
-    		}
-    	}
-    	
-    	return ans;
     }
 }
