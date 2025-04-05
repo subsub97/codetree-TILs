3e63dd32ba2aa6276b752473fd0e09693c71c00c
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2025-04-06 04:02:30 +0900

    Title: 메두사와 전사들, Time: 93ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250406/메두사와 전사들/medusa-and-warriors.java
+++ 250406/메두사와 전사들/medusa-and-warriors.java
@@ -240,12 +240,12 @@ public class Main {
                 if(counts[i][c] > 0 && stoneMemo[i][c] == 0) {
                     // c를 기준으로 전파
                     if(curR > i) { // 메두사보다 위에 있는 경우
-                        checkSafeArea(r, c - 1, i - 1, dir, 0, 1);
+                        checkSafeArea(r, c - 1, i - 1, dir, 0, 2);
                     } else if (curC == i) { // 동일 선상인 경우
-                        checkSafeArea(r, c - 1, i, dir, 1, 0);
+                        checkSafeArea(r, c - 1, i, dir, 1, 1);
                     }
                     else{ // 메두사보다 아래 있는 경우
-                        checkSafeArea(r, c- 1, i, dir, 2, 1);
+                        checkSafeArea(r, c- 1, i, dir, 2, 2);
                     }
                     sum += counts[i][c];
                 }
@@ -260,12 +260,12 @@ public class Main {
                 if(counts[i][c] > 0 && stoneMemo[i][c] == 0) {
                     // c를 기준으로 전파
                     if(curR > i) { // 메두사보다 위에 있는 경우
-                        checkSafeArea(r, c + 1, i - 1, dir, 0, 1);
+                        checkSafeArea(r, c + 1, i - 1, dir, 0, 2);
                     } else if (curC == i) { // 동일 선상인 경우
-                        checkSafeArea(r, c + 1, i, dir, 1, 0);
+                        checkSafeArea(r, c + 1, i, dir, 1, 1);
                     }
                     else{ // 메두사보다 아래 있는 경우
-                        checkSafeArea(r, c + 1, i, dir, 2, 1);
+                        checkSafeArea(r, c + 1, i, dir, 2, 2);
                     }
                     sum += counts[i][c];
                 }
